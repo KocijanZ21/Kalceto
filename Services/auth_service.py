eda777bb09ef2a85c51740ec83bdeedaa0b5102f
Vanja Kalaković <vk4471@student.uni-lj.si>
2024-07-29 11:02:44 +0200

    dodala funkcije za igralca in sodnika

--- Services/auth_service.py
+++ Services/auth_service.py
@@ -61,3 +61,46 @@ class AuthService:
 
         return UporabnikDto(username=uporabnik, role=rola)
     
+    def dodaj_igralca(self, emso: str, ime: str, priimek: str, spol: str, drzava: str, email: str, rojstni_dan: str) -> igralec:
+        i = igralec(
+            emso = emso,
+            ime = ime,
+            priimek=priimek,
+            spol=spol,
+            drzava=drzava,
+            email=email,
+            rojstni_dan=rojstni_dan
+        )
+        try:
+            self.repo.dodaj_igralca(i)
+            return True
+        except:
+            return False
+        
+
+    def dobi_igralca(self, emso: str) -> igralec:
+        igralec = self.repo.dobi_igralca(emso)
+        return igralec
+    
+    def odstrani_igralca(self, emso: str):
+        self.repo.odstrani_igralca(emso)
+
+    def dodaj_sodnika(self, emso: str, ime: str, priimek: str) -> sodnik:
+        s = sodnik(
+            emso=emso,
+            ime=ime,
+            priimek=priimek
+        )
+        try:
+           self.repo.dodaj_sodnika(s)
+           return True
+        except:
+           return False
+
+    def dobi_sodnika(self, emso: str)  -> sodnik:
+        sodnik = self.repo.dobi_sodnika(emso) 
+        return sodnik
+
+    def odstrani_sodnika(self, emso: str):
+        self.repo.odstrani_sodnika(emso)    
+    
